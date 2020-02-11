c6c08365e6756d3377f9e8abad221f89194c1670
user <user@this.com>
2020-02-11 14:40:35 -0500

    git init

---
+++ src/main/java/com/bah/msd/BoozAllenApplication.java
@@ -0,0 +1,13 @@
+package com.bah.msd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoozAllenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoozAllenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bah/msd/BoozAllenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bah.msd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoozAllenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
