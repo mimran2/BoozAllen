d7a0dc67f420f5872a52e8c9feb60f694e778b09
user <user@this.com>
2020-02-12 16:18:55 -0500

    remove controllers classs update eventsAPI and Registration.API

--- src/main/java/com/bah/msd/api/EventsAPI.java
+++ src/main/java/com/bah/msd/api/EventsAPI.java
@@ -1,5 +1,56 @@
 package com.bah.msd.api;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bah.msd.entities.Event;
+import com.bah.msd.persistence.EventRepository;
+
+@RestController
+@RequestMapping("/api")
 public class EventsAPI {
+	@Autowired
+	private EventRepository EventRepository;
+	
+	private final String JSON = "application/json";
+
+	
+	@GetMapping(path = "/events", produces = JSON)
+	public Iterable<Event> getAll(HttpServletResponse response) {
+		return EventRepository.findAll();
+	}
+	
+	@PutMapping(path = "/events/{id}")
+	public ResponseEntity<?> createUpdateEvent(@PathVariable long id, 
+			@RequestBody Event requestEvent) {
+		if(requestEvent.getCode()== null || requestEvent.getDescription() == null
+			||requestEvent.getTitle() == null) {
+			return ResponseEntity.badRequest().build();
+		}
+		requestEvent = EventRepository.save(requestEvent);
+		
+		return ResponseEntity.ok().build();
+	}
+	
+	@DeleteMapping(path = "/events/{id}")
+	@ResponseBody
+	public ResponseEntity<?> deleteEvent(@PathVariable long id) {
+		try {
+			EventRepository.deleteById(id);
+		} catch (IllegalArgumentException e) {
+			return ResponseEntity.badRequest().build();
+		}
+		return ResponseEntity.ok().build();
+		}
 
 }
--- src/main/java/com/bah/msd/api/RegistrationsAPI.java
+++ src/main/java/com/bah/msd/api/RegistrationsAPI.java
@@ -1,5 +1,36 @@
 package com.bah.msd.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bah.msd.entities.Registration;
+import com.bah.msd.persistence.RegistrationRepository;
+
+@RestController
+@RequestMapping("/api")
 public class RegistrationsAPI {
+	
+	@Autowired
+	private RegistrationRepository RegistrationRepository;
+	
+	@GetMapping(path = "/registrations")
+	public Iterable<Registration> getRegistrations() {
+		return RegistrationRepository.findAll();
+	}
+	
+	@GetMapping(path = "/registrations/{id}")
+	public ResponseEntity<?> deleteRegistrationById(@PathVariable long id) {
+		try {
+			RegistrationRepository.deleteById(id);
+		}catch(IllegalArgumentException e) {
+			return  ResponseEntity.badRequest().build();
+		}
+		return ResponseEntity.ok().build();
+	}
+	
 
 }
