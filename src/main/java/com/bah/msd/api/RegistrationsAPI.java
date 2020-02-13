ab981391f6e02e6908a0b7fdef7760f853ad8c9c
user <user@this.com>
2020-02-13 16:18:21 -0500

    Registration API

--- src/main/java/com/bah/msd/api/RegistrationsAPI.java
+++ src/main/java/com/bah/msd/api/RegistrationsAPI.java
@@ -5,6 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,24 +16,33 @@ import com.bah.msd.persistence.RegistrationRepository;
 @RestController
 @RequestMapping("/api")
 public class RegistrationsAPI {
-	
+
 	@Autowired
 	private RegistrationRepository RegistrationRepository;
-	
+
 	@GetMapping(path = "/registrations")
 	public Iterable<Registration> getRegistrations() {
 		return RegistrationRepository.findAll();
 	}
-	
+
+	@PutMapping(path = "/registrations/{id}")
+	public ResponseEntity<?> createUpdateRegistration(@PathVariable long id, @RequestBody Registration requestEvent) {
+		if (requestEvent.getNotes() == null || requestEvent.getRegistration_date() == null) {
+			return ResponseEntity.badRequest().build();
+		}
+		requestEvent = RegistrationRepository.save(requestEvent);
+
+		return ResponseEntity.ok().build();
+	}
+
 	@DeleteMapping(path = "/registrations/{id}")
 	public ResponseEntity<?> deleteRegistrationById(@PathVariable long id) {
 		try {
 			RegistrationRepository.deleteById(id);
-		}catch(IllegalArgumentException e) {
-			return  ResponseEntity.badRequest().build();
+		} catch (IllegalArgumentException e) {
+			return ResponseEntity.badRequest().build();
 		}
 		return ResponseEntity.ok().build();
 	}
-	
 
 }
